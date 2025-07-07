ed31ead1fa2d00217b599e463967d891ff6c9e49
oceaneyessss <mwu119@uottawa.ca>
2025-07-06 20:35:55 -0400

    ehh, reran build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/SEG3125_A4_Group66/',
-  plugins: [react()],
+  base: './',
+  plugins: [react()]
 })
