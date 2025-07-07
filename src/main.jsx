8d2bec21ea7b260406be34045e54e7cc6078c0c4
oceaneyessss <mwu119@uottawa.ca>
2025-07-06 20:44:53 -0400

    main changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
-import App from './App'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min' 
+import './index.css'
+import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(
   <HashRouter>
     <App />
   </HashRouter>
